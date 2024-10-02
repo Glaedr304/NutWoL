88c92014b41c92b54d7da9bbbfa42ccd46a7bd16
Glaedr304 <Davidfrey304@gmail.com>
2024-10-01 22:50:48 -0500

    save point

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import PyNUTClient
-from pprint import pprint
+# import logging
 
-client = PyNUTClient.PyNUT.PyNUTClient(host = "221.111.16.123")
+logName = 'myapp.log'
+
+host = "221.111.16.123"
+host = "nightblood"
+
+# logger = logging.getLogger(__name__)
+# logging.basicConfig(filename=logName, format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+history = open(logName, "r+")
+
+oldBatteryPercentage = history.readline()
+
+client = PyNUTClient.PyNUT.PyNUTClient(host = host)
 myList = client.GetUPSNames()
 
-for myName in myList:
-    vars = client.GetUPSVars(ups = myName)
-    pprint(vars)
+for thisUPSName in myList:
+    vars = client.GetUPSVars(ups = thisUPSName)
+    batteryPercentage = vars[b'battery.charge'].decode('utf-8')
+    # logger.info("Battery Percentage: %s", batteryPercentage)
+    history.seek(0)
+    history.write(batteryPercentage)
+    history.truncate()
