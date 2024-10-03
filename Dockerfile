9cb5ef79bd5b1e95971f04c4f34cf7c7d198d471
Glaedr304 <Davidfrey304@gmail.com>
2024-10-02 23:05:17 -0500

    Refactoring, and practicing with logging

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10-slim
+
+# Set the working directory
+WORKDIR /usr/src/app
+
+# Copy the current directory contents into the container at /usr/src/app
+COPY . .
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Define environment variables
+ENV LOGGING_LEVEL=INFO
+ENV UPS_IP=127.0.0.1
+ENV SLEEPER_LIST
+ENV BATT_THRESHOLD=30
+ENV DELAY=120
+
+# Run the application
+CMD ["python", "./main.py"]
--- main.py
+++ main.py
@@ -1,25 +1,60 @@
-import PyNUTClient
-# import logging
+import PyNUTClient, os, time, logging
+from wakeonlan import send_magic_packet
 
 logName = 'myapp.log'
 
-host = "221.111.16.123"
-host = "nightblood"
+logger = logging.getLogger(__name__)
 
-# logger = logging.getLogger(__name__)
-# logging.basicConfig(filename=logName, format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+host = os.getenv("UPS_IP", "127.0.0.1")
+logger.debug('host is %s', host)
 
-history = open(logName, "r+")
+sleepers = os.getenv("SLEEPER_LIST", "a0:36:9f:50:a1:e1").split(",")
+logger.debug('Mac Addresses to wake: %s', sleepers)
 
-oldBatteryPercentage = history.readline()
+batteryThreshold = int(os.getenv("BATT_THRESHOLD", "30"))
+logger.debug('Battery Threshold %s', batteryThreshold)
+
+sleepDelay = int(os.getenv("DELAY", "120"))
+logger.debug('Sleep delay: %s', sleepDelay)
+
+logLevel = eval('logging.' + os.getenv("LOGGING_LEVEL", "DEBUG").upper())
+logger.info('Log level is %s', logLevel)
+
+host = "nightblood" # TODO Remove this line.
+
+logging.basicConfig( 
+    format = '%(asctime)s %(levelname)-8s %(message)s', 
+    level = logLevel, 
+    datefmt = '%Y-%m-%d %H:%M:%S'
+    )
 
 client = PyNUTClient.PyNUT.PyNUTClient(host = host)
 myList = client.GetUPSNames()
 
-for thisUPSName in myList:
-    vars = client.GetUPSVars(ups = thisUPSName)
-    batteryPercentage = vars[b'battery.charge'].decode('utf-8')
-    # logger.info("Battery Percentage: %s", batteryPercentage)
-    history.seek(0)
-    history.write(batteryPercentage)
-    history.truncate()
+def main():
+    with open(logName, "r+") as history:
+
+        oldBatteryPercentage = history.readline()
+        logger.debug('Previous Battery Percentage %s', oldBatteryPercentage)
+
+        for thisUPSName in myList:
+            vars = client.GetUPSVars(ups = thisUPSName)
+            batteryPercentage = vars[b'battery.charge'].decode('utf-8')
+            logger.debug('This UPS (%s) has a batttery percentage of %s', thisUPSName, batteryPercentage)
+
+        history.seek(0)
+        history.write(batteryPercentage)
+        history.truncate()
+
+    if oldBatteryPercentage < batteryPercentage and batteryPercentage >= batteryThreshold:
+        try:
+            send_magic_packet(sleepers) # TODO this wont work if sleepers is a list
+        except Exception as e:
+            logger.error('An Error Occured when sending the magic packet. Error: %s', e)    
+        else:
+            logger.debug('The magic packet was sent without error.')
+
+while __name__ == "__main__":
+    main()
+    logger.debug('Sleeping for %s seconds', sleepDelay) 
+    time.sleep(sleepDelay)
